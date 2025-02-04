38468b6bdb300ff17b560e88f29074e12ca03ded
SHAIK MOHAMMAD BASHID <70657442+MohammadBashid@users.noreply.github.com>
2025-02-04 10:55:13 +0000

    requested to cors

--- my-backend/server.js
+++ my-backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
+
+ // This will allow all origins to make requests to your back-end
+app.use(cors());
 
 // Middleware to parse incoming JSON data
 app.use(express.json());
